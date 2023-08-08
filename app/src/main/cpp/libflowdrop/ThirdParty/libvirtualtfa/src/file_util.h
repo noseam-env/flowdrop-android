79bca7aa797fee5930fcf083f30ea01e0827e0d4
Nelonn <thenelonn@gmail.com>
2023-08-08 04:22:44 +0300

    Fixed submodules

--- app/src/main/cpp/libflowdrop/ThirdParty/libvirtualtfa/src/file_util.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-#include <filesystem>
-
-void setFileMetadata(const char *filepath, std::filesystem::perms mode, std::uint64_t ctime, std::uint64_t mtime);
--- app/src/main/cpp/libflowdrop/src/portroller.hpp
+++
@@ -1,9 +0,0 @@
-/*
- * This file is part of libflowdrop.
- *
- * For license and copyright information please follow this link:
- * https://github.com/noseam-env/libflowdrop/blob/master/LEGAL
- */
-#pragma once
-
-unsigned short rollAvailablePort(unsigned short defaultPort);
